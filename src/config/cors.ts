429866bd9ccd0c63bd117e50a9632069f8993873
Jordi Romero <jordirofu@gmail.com>
2026-04-21 14:35:45 +0200

    CorsConfig modified

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -14,18 +14,12 @@ import { CorsOptions } from 'cors'
 // }
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
-        // Obtenemos la URL de la variable y eliminamos espacios en blanco por si acaso
-        const frontendUrl = process.env.FRONTEND_URL?.trim();
-        
-        console.log('--- CORS Check ---');
-        console.log('Origin de la petición:', origin);
-        console.log('Variable FRONTEND_URL:', frontendUrl);
-
-        // Permitimos si no hay origen (Postman/Server-side) o si coincide
+        const frontendUrl = process.env.FRONTEND_URL;
+        // Permitimos si no hay origin (como Postman) o si coincide con Vercel
         if (!origin || origin === frontendUrl) {
             callback(null, true);
         } else {
-            callback(new Error('Error de CORS: Origen no permitido'));
+            callback(new Error('Error de CORS'));
         }
     }
 }
